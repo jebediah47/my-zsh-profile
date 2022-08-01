6ceaa3f869b5f6f51891875bece50533a896735d
Christian Llupo <jebediah47@protonmail.com>
2022-08-01 18:52:18 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 # Make plugins folder
 mkdir -p ~/.zsh/plugins
+mkdir ~/.zsh/plugins/zsh-syntax-highlighting
+mkdir ~/.zsh/plugins/zsh-autocomplete
 
 # Clone and install pfetch
 cd ~/Downloads
@@ -22,12 +24,12 @@ curl -sS https://starship.rs/install.sh | sh
 
 # Clone plugins to ~/.zsh dir
 ## Starting with zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/plugins/zsh-syntax-highlighting
 
 ## Then with zsh-autocomplete
-git clone https://github.com/marlonrichert/zsh-autocomplete ~/.zsh/plugins
+git clone https://github.com/marlonrichert/zsh-autocomplete ~/.zsh/plugins/zsh-autocomplete
 
 
 ## Clone my profile to ~ dir
-cp .zshrc ~
-
+cd ~
+wget -c https://raw.githubusercontent.com/jebediah47/my-zsh-profile/main/.zshrc
